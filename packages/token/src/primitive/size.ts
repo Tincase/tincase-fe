c51e615d681ac1b7bce7a6c66e06053ccd339886
m-dzn <mdzn.dev@gmail.com>
2023-03-19 14:44:18 +0900

    💄 (token) Add size primitive tokens

---
+++ packages/token/src/primitive/size.ts
@@ -0,0 +1,46 @@
+/*
+ * This code is based on the `spacing.ts` from `chakra-ui`.
+ * See https://github.com/chakra-ui/chakra-ui for more information.
+ * Licensed under the MIT License.
+ */
+
+// prettier-ignore
+export const size = {
+  0:        "0",
+  1:        "0.0625rem",
+  2:        "0.125rem",
+  4:        "0.25rem",
+  6:        "0.375rem",
+  8:        "0.5rem",
+  10:       "0.625rem",
+  12:       "0.75rem",
+  14:       "0.875rem",
+  16:       "1rem",
+  18:       "1.125rem",
+  20:       "1.25rem",
+  24:       "1.5rem",
+  28:       "1.75rem",
+  32:       "2rem",
+  36:       "2.25rem",
+  40:       "2.5rem",
+  44:       "2.75rem",
+  48:       "3rem",
+  52:       "3.25rem",
+  56:       "3.5rem",
+  60:       "3.75rem",
+  64:       "4rem",
+  68:       "4.25rem",
+  72:       "4.5rem",
+  76:       "4.75rem",
+  80:       "5rem",
+  96:       "6rem",
+  112:      "7rem",
+  128:      "8rem",
+  144:      "9rem",
+  160:     "10rem",
+  240:     "15rem",
+  320:     "20rem",
+  400:     "25rem",
+  1024:    "64rem",
+  max:   "9999px",
+} as const;
