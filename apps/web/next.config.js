f6a1792bbcb21af7a824ee29b8f82924a23296ff
m-dzn <mdzn.dev@gmail.com>
2023-03-25 17:03:59 +0900

    ➕ (web) Add next-i18next Internationalization lib.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,7 @@
+const { i18n } = require('./next-i18next.config');
+
 module.exports = {
   reactStrictMode: true,
-  transpilePackages: ["@tincase/ui"],
+  transpilePackages: ['@tincase/ui'],
+  i18n,
 };
