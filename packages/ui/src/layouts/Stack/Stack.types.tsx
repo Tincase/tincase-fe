51680bbd2fc49674391f948e7b053563528aba7d
m-dzn <mdzn.dev@gmail.com>
2023-03-26 17:28:56 +0900

    🏷️ (ui) Extend Stack component to React.HTMLAttributes

--- packages/ui/src/layouts/Stack/Stack.types.tsx
+++ packages/ui/src/layouts/Stack/Stack.types.tsx
@@ -3,11 +3,13 @@ import { HorizontalAlign, Spacing, VerticalAlign } from '@tincase/token';
 // prettier-ignore
 export interface StackOptions {
   direction?:         'row' | 'column';
-  gap:                keyof Spacing;
+  gap?:               keyof Spacing;
   horizontalAlign?:   HorizontalAlign;
   verticalAlign?:     VerticalAlign;
 }
 
-export interface StackProps extends StackOptions {
+export interface StackProps
+  extends StackOptions,
+    React.HTMLAttributes<HTMLDivElement> {
   children?: React.ReactNode;
 }
